c952ac5ca27d90023b600f7ba07b0872bbb1866a
Grzegorz Bokota <bokota+github@gmail.com>
2024-09-01 14:14:44 +0200

    fix if condition in fetching data from npe2api

--- src/napari_dashboard/utils.py
+++ src/napari_dashboard/utils.py
@@ -5,7 +5,7 @@ def requests_get(url: str, depth: int = 10):
     """this is util functiosn to workaround problem with status 500 in vercell"""
     for _ in range(depth):
         res = requests.get(url)
-        if res.status_code == 500:
-            continue
+        if res.status_code != 500:
+            return res
         time.sleep(1) # wait
     raise RuntimeError(f"Failed to get data from the server for {url}")
